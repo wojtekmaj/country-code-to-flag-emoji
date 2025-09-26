b08006bf913a553cdb0e5cf806199907a4ae0b5d
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-09-26 16:09:24 +0200

    Fix compatibility with isolatedDeclarations

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+import type { ViteUserConfig } from 'vitest/config';
+
+const config: ViteUserConfig = defineConfig({
   test: {
     watch: false,
   },
 });
+
+export default config;
